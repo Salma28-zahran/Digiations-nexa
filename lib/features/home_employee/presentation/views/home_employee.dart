bdd2f556183b81ce403da5ab22e19c6d708241cf
salma zahran <salmazahran14@gmail.com>
2026-01-13 13:51:52 +0200

    correct flow

---
+++ lib/features/home_employee/presentation/views/home_employee.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeEmployee extends StatelessWidget {
+  const HomeEmployee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
